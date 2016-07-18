bb78bb97375b0fc313f83e5a65b8f14cdb8ba2f8
alberto__segura <alberto.segura.delgado@gmail.com>
2016-07-18 19:21:04 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+
+// Get config from enviroment vars
+var PORT = process.env.PORT || 3000;
+var URI = process.env.URI || "localhost";
+
+var USERNAME = process.env.USERNAME || "0";
+var PASSWORD = process.env.PASSWORD || "0";
+var PROVIDER = process.env.PROVIDER || "google";
+
+// Libs for HTTP Server (Web)
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+// Requests
+var request = require('request');
+
+var async = require('async');
+
+// Initialize HTTP Server
+app.use(express.static('public_html'));
+
+
+// Pokemon Go Lib
+var Pokeio = require('pokemon-go-node-api')
+
+// Listen on port
+http.listen(PORT, function(){
+	console.log('listening on *:'+ PORT);
+});
+
+
+// Initialize PokemonGo
+
+app.get('/api/start/:lng/:lat', function(req, res){
+
+    var location = {
+        type: 'coords',
+        latitude: req.params.lat,
+        longitude: req.params.lng
+    };
+
+    Pokeio.init(USERNAME, PASSWORD, location, provider, function(err) {
+        if (err) throw err;
+
+        console.log('[i] Current location: ' + Pokeio.playerInfo.locationName);
+        console.log('[i] lat/long/alt: ' + Pokeio.playerInfo.latitude + ' ' + Pokeio.playerInfo.longitude + ' ' + Pokeio.playerInfo.altitude);
+
+        Pokeio.GetProfile(function(err, profile) {
+            if (err) throw err;
+
+            console.log('[i] Username: ' + profile.username);
+            console.log('[i] Poke Storage: ' + profile.poke_storage);
+            console.log('[i] Item Storage: ' + profile.item_storage);
+
+            var poke = 0;
+            if (profile.currency[0].amount) {
+                poke = profile.currency[0].amount;
+            }
+
+            console.log('[i] Pokecoin: ' + poke);
+            console.log('[i] Stardust: ' + profile.currency[1].amount);
+
+        });
+    });
+
+});
+
+
---
+++ public_html/js/index.js
@@ -0,0 +1,63 @@
+
+
+var MAP  = null;
+
+
+function initMap() {
+	/*
+	map = new google.maps.Map(document.getElementById('map'), {
+	center: {lat: -34.397, lng: 150.644},
+	zoom: 8
+	});
+	*/
+
+	getLocation(function(pos){
+		var lng = pos.coords.longitude;
+		var lat = pos.coords.latitude;
+
+		localStorage.setItem("lng", lng);
+		localStorage.setItem("lat", lat);
+		showMap(lat, lng);
+	});
+}
+
+
+function getLocation(cb)
+{
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(cb);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+
+function showMap(lat, lng)
+{
+	MAP = new google.maps.Map(document.getElementById('map'), {
+	  center: {lat: lat, lng: lng},
+	  zoom: 20
+	});
+	var marker = new google.maps.Marker({
+		position: {lat: lat, lng: lng},
+		label: "",
+		map: MAP
+	});
+}
+
+
+function getRoute(start, end)
+{
+	var request = {
+		origin:start,
+		destination:end,
+		travelMode: google.maps.TravelMode.DRIVING
+	};
+	directionsService.route(request, function(response, status) {
+		if (status == google.maps.DirectionsStatus.OK)
+		{
+			console.log(status);
+		}
+	});
+}
+
